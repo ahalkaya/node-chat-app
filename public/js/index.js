8c0a0bf5b7c0df808158c7f35a21e285504dc5cd
ahalkaya <alkaya.alihan@gmail.com>
2017-08-28 14:58:11 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Alihan',
-        text: 'Hey. This works for me.'
-    });
 });
 
 socket.on('disconnect', function () {
