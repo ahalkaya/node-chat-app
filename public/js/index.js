f0849dd3153ac1371c265dc01de0ef8f4187bbca
ahalkaya <alkaya.alihan@gmail.com>
2017-08-30 18:00:36 +0300

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -16,13 +16,6 @@ socket.on('newMessage', function (message) {
     jQuery('#messages').append(li);
 });
 
-socket.emit('createMessage', {
-    from: 'Mr. Brown',
-    text: 'Hello'
-}, function (data) { // aknowledgement
-    console.log('Got it!', data);
-});
-
 socket.on('newLocationMessage', function (message) {
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My current location</a>');
@@ -36,11 +29,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
+    var messageTextbox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
-    }, function () {
-
+        text: messageTextbox.val()
+    }, function () { // aknowledgement
+        messageTextbox.val('');
     });
 });
 
@@ -50,12 +45,16 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by your browser.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch the location.');
     });
 });
